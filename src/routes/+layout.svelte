866cb3b99da46778ba4425007053f9f7da724b13
Eggrror404 <94736451+Eggrror404@users.noreply.github.com>
2023-04-26 23:06:45 +0800

    Change formatting

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,102 +1,102 @@
 <script>
-  import vitamin from "$lib/vitamin-d.png"
+  import vitamin from "$lib/vitamin-d.png";
 </script>
 
 <header role="banner">
-	<nav>
-		<div id="nav-header">
-			<img src={vitamin} alt="" />
-			<span class="header-text">
-				<span class="l1">新樓醫院心臟科附屬</span>
-				<br />
-				<span class="l2">電機社</span>
-			</span>
-		</div>
-		<ul id="nav-links">
-			<li><a href="#">主頁</a></li>
-			<li><a href="#">ABOUT</a></li>
-		</ul>
-	</nav>
+  <nav>
+    <div id="nav-header">
+      <img src={vitamin} alt="" />
+      <span class="header-text">
+        <span class="l1">新樓醫院心臟科附屬</span>
+        <br />
+        <span class="l2">電機社</span>
+      </span>
+    </div>
+    <ul id="nav-links">
+      <li><a href="#">主頁</a></li>
+      <li><a href="#">ABOUT</a></li>
+    </ul>
+  </nav>
 </header>
 
 <slot />
 
 <style>
-	:global(:root) {
-		font-family: Inter, system-ui, Avenir, Helvetica, Arial, sans-serif;
-		line-height: 1.5;
-		font-weight: 400;
+  :global(:root) {
+    font-family: Inter, system-ui, Avenir, Helvetica, Arial, sans-serif;
+    line-height: 1.5;
+    font-weight: 400;
 
-		color-scheme: light dark;
-		color: rgba(255, 255, 255, 0.87);
-		background-color: #242424;
+    color-scheme: light dark;
+    color: rgba(255, 255, 255, 0.87);
+    background-color: #242424;
 
-		font-synthesis: none;
-		text-rendering: optimizeLegibility;
-		-webkit-font-smoothing: antialiased;
-		-moz-osx-font-smoothing: grayscale;
-		-webkit-text-size-adjust: 100%;
-	}
+    font-synthesis: none;
+    text-rendering: optimizeLegibility;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    -webkit-text-size-adjust: 100%;
+  }
 
-	:global(body) {
-		margin: 0;
-		/* min-width: 320px; */
-		/* min-height: 100vh; */
-	}
+  :global(body) {
+    margin: 0;
+    /* min-width: 320px; */
+    /* min-height: 100vh; */
+  }
 
-	:global(a) {
-		font-weight: 500;
-		color: inherit;
-		text-decoration: inherit;
-	}
-	:global(a:hover) {
-		color: #535bf2;
-	}
-	header {
-		display: flex;
-		justify-content: center;
-		padding: 1.5em 3.5em;
-	}
+  :global(a) {
+    font-weight: 500;
+    color: inherit;
+    text-decoration: inherit;
+  }
+  :global(a:hover) {
+    color: #535bf2;
+  }
+  header {
+    display: flex;
+    justify-content: center;
+    padding: 1.5em 3.5em;
+  }
 
-	header nav {
-		display: flex;
-		flex-direction: row;
-		flex-grow: 1;
-		align-items: center;
-		max-width: 50em;
-	}
+  header nav {
+    display: flex;
+    flex-direction: row;
+    flex-grow: 1;
+    align-items: center;
+    max-width: 50em;
+  }
 
-	#nav-header {
-		margin-right: auto;
-	}
+  #nav-header {
+    margin-right: auto;
+  }
 
-	#nav-header img {
-		margin-right: 1em;
-		height: 3.4em;
-	}
+  #nav-header img {
+    margin-right: 1em;
+    height: 3.4em;
+  }
 
-	#nav-header .header-text {
-		display: inline-block;
-	}
+  #nav-header .header-text {
+    display: inline-block;
+  }
 
-	.header-text .l1 {
-		font-size: 100%;
-		white-space: nowrap;
-	}
+  .header-text .l1 {
+    font-size: 100%;
+    white-space: nowrap;
+  }
 
-	.header-text .l2 {
-		font-size: 180%;
-		white-space: nowrap;
-	}
+  .header-text .l2 {
+    font-size: 180%;
+    white-space: nowrap;
+  }
 
-	#nav-links {
-		list-style: none;
-		margin: 0;
-	}
+  #nav-links {
+    list-style: none;
+    margin: 0;
+  }
 
-	#nav-links li {
-		display: inline;
-		font-size: 110%;
-		margin-left: 2em;
-	}
+  #nav-links li {
+    display: inline;
+    font-size: 110%;
+    margin-left: 2em;
+  }
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,28 +1,28 @@
 <script lang="ts">
-	let n: number = 0;
+  let n: number = 0;
 </script>
 
 <section class="banner">
-	<div class="banner-img" />
+  <div class="banner-img" />
 </section>
 
 <section class="content">
-	游😶育是甲，這是真的!<br />
+  游😶育是甲，這是真的!<br />
 
-	<button on:click={() => n++}>猜謎: 南一中第一甲是誰?</button>
+  <button on:click={() => n++}>猜謎: 南一中第一甲是誰?</button>
 
-	{#each Array(n) as _}
-		<br /> 游😲育
-	{/each}
+  {#each Array(n) as _}
+    <br /> 游😲育
+  {/each}
 </section>
 
 <style>
-	.banner-img {
-		background: url('$lib/stormseeker-rX12B5uX7QM-unsplash.jpg') fixed 0 65%;
-		width: 100%;
-		height: 20em;
-		box-shadow: inset 0 0 50px 2px black;
-	}
+  .banner-img {
+    background: url("$lib/stormseeker-rX12B5uX7QM-unsplash.jpg") fixed 0 65%;
+    width: 100%;
+    height: 20em;
+    box-shadow: inset 0 0 50px 2px black;
+  }
 
   .content {
     margin: 3em 10em;
