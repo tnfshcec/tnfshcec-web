6537b3ed88997a103815175a724452722f183ad6
Eggrror404 <94736451+Eggrror404@users.noreply.github.com>
2023-06-05 18:09:07 +0800

    setup github workflow
    Github Workflow for static site generation hosted on Github Pages
    From `kit.svelte.dev`

--- svelte.config.js
+++ svelte.config.js
@@ -4,6 +4,8 @@ import preprocess from "svelte-preprocess";
 import adapter from "@sveltejs/adapter-auto";
 import { vitePreprocess } from "@sveltejs/kit/vite";
 
+const dev = process.argv.includes("dev");
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   extensions: [".svelte", ...mdsvexConfig.extensions],
@@ -22,7 +24,10 @@ const config = {
     // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
     // If your environment is not supported or you settled on a specific environment, switch out the adapter.
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
-    adapter: adapter()
+    adapter: adapter(),
+    paths: {
+      base: dev ? "" : process.env.BASE_PATH
+    }
   }
 };
 
