cf0b4fa892deb5d8899849c5dc7c489ed129b8e2
Eggrror404 <94736451+Eggrror404@users.noreply.github.com>
2023-07-29 18:34:22 +0800

    revert post `load` from last commit for prerender

--- src/routes/post/[...post]/+page.ts
+++
@@ -1,6 +0,0 @@
-import type { PageLoad } from "./$types";
-import { base } from "$app/paths";
-
-export const load = (async ({ fetch, params }) => {
-  return fetch(`${base}/post/${params.post}/endpoint`).then((r) => r.json());
-}) satisfies PageLoad;
