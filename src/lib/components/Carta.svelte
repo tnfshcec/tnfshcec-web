a20bf0c1ba399b87c446237c24d304d0e2524dc0
Eggrror404 <94736451+Eggrror404@users.noreply.github.com>
2023-12-07 00:13:38 +0800

    add spoiler element extension

--- src/lib/components/Carta.svelte
+++ src/lib/components/Carta.svelte
@@ -1,18 +1,19 @@
 <script lang="ts">
   import { Carta, CartaEditor, CartaViewer, type CartaExtension } from "carta-md";
   import { code } from "@cartamd/plugin-code";
-  import markedFootnote from 'marked-footnote'
+  import markedFootnote from "marked-footnote";
+  import markedSpoiler from "$lib/utils/markedSpoiler";
 
   import "carta-md/default.css"; /* Default theme */
   import "carta-md/light.css"; /* Markdown input theme */
-  import "@cartamd/plugin-code/default.css"
+  import "@cartamd/plugin-code/default.css";
 
   export let value: string;
   export let type: "viewer" | "editor" = "editor";
 
   const cartaExt: CartaExtension = {
-    markedExtensions: [markedFootnote()]
-  }
+    markedExtensions: [markedFootnote(), markedSpoiler]
+  };
 
   const carta = new Carta({
     extensions: [code(), cartaExt]
