36a5ff3f6140e796ac698fd0ccebff7b06cec56e
Eggrror404 <94736451+Eggrror404@users.noreply.github.com>
2023-12-20 18:49:28 +0800

    format file, imporve mobile view

--- src/routes/2048/+page.svelte
+++ src/routes/2048/+page.svelte
@@ -5,7 +5,7 @@
   import { Game2048, getController } from "./2048";
   import type { KeyboardEventHandler } from "svelte/elements";
   import Restart from "~icons/mdi/restart";
-  import "./2048_dark.css"
+  import "./2048_dark.css";
 
   const game = new Game2048();
   const controller = getController(game);
@@ -51,8 +51,9 @@
   <div class="w-full max-w-screen-xl select-none">
     <PageTitle current="2048" />
     <div class="flex flex-col justify-evenly gap-4 p-4 md:flex-row">
-      <div class="space-y-2">
-        <div class="flex gap-2">
+      <!-- left panel (scores & restart) -->
+      <div class="space-y-4">
+        <div class="flex flex-wrap gap-2">
           <div class="min-w-[8rem] rounded-sm bg-secondary p-2">
             <span class="text-lg">SCORE</span><br />
             <span class="text-2xl font-bold">{score}</span>
@@ -67,22 +68,28 @@
           RESTART
         </button>
       </div>
+
+      <!-- actual game -->
       <div
         id="stage"
-        class="relative h-[450px] w-[450px] rounded border border-text"
+        class="relative h-[450px] w-[450px] flex-shrink-0 rounded border border-text"
         on:touchstart|preventDefault={(e) =>
           controller.start(e.touches[0].clientX, e.touches[0].clientY)}
         on:touchmove|preventDefault={(e) =>
           controller.move(e.touches[0].clientX, e.touches[0].clientY)}
         on:touchend|preventDefault={(e) => controller.end()}
       />
-      <div class="">
+
+      <!-- right text -->
+      <div>
         <span class="font-bold">這裡為什麼有2048？</span><br />
-        <span class="">我怎麼會知道。</span><br /><br />
+        <span>我怎麼會知道。</span><br /><br />
         <span class="font-bold">2048怎麼玩？</span><br />
-        <span class="">自己Google。</span><br /><br />
+        <span>自己Google。</span><br /><br />
         <span class="font-bold">但是我要怎麼控制？</span><br />
-        <span class="">方向鍵或直接滑動，滑鼠或觸控都可以。</span>
+        <span>方向鍵或直接滑動，滑鼠或觸控都可以。</span><br /><br />
+        <span class="font-bold">這邊在供三小？</span><br />
+        <span>不知道欸，我只是想放點字讓頁面不要這麼空。</span>
       </div>
     </div>
   </div>
@@ -119,62 +126,63 @@
   }
 
   :global(.num2) {
-    background: #EEE4DA;
-    color: #776E65;
+    background: #eee4da;
+    color: #776e65;
   }
 
   :global(.num4) {
-    background: #EDE0C8;
-    color: #776E65;
+    background: #ede0c8;
+    color: #776e65;
   }
 
   :global(.num8) {
-    background: #F2B179;
-    color: #F9F6F2;
+    background: #f2b179;
+    color: #f9f6f2;
   }
 
   :global(.num16) {
-    background: #F59563;
-    color: #F9F4E7;
+    background: #f59563;
+    color: #f9f4e7;
   }
 
   :global(.num32) {
-    background: #F67C60;
-    color: #F9F6F2;
+    background: #f67c60;
+    color: #f9f6f2;
   }
 
   :global(.num64) {
-    background: #F65E3B;
-    color: #F9F6F2;
+    background: #f65e3b;
+    color: #f9f6f2;
   }
 
   :global(.num128) {
-    background: #EDCF73;
-    color: #F9F6F2;
+    background: #edcf73;
+    color: #f9f6f2;
   }
 
   :global(.num256) {
-    background: #EDCC62;
-    color: #F9F6F2;
+    background: #edcc62;
+    color: #f9f6f2;
   }
 
   :global(.num512) {
-    background: #EDC850;
+    background: #edc850;
     color: #fb4e4e;
   }
 
   :global(.num1024) {
     background: #6483b5;
-    color: #F9F6F2;
+    color: #f9f6f2;
   }
 
   :global(.num2048) {
-    background: #EDC53F;
-    color: #F9F6F2;
+    background: #edc53f;
+    color: #f9f6f2;
   }
 
   :global(.num4096) {
-    background: #EDC22D;
-    color: #F9F6F2;
+    background: #edc22d;
+    color: #f9f6f2;
   }
 </style>
+
