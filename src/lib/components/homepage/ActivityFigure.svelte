e6f3b7181d3283ea2c2cf907e68b11e0bd0d6e7e
Eggrror404 <94736451+Eggrror404@users.noreply.github.com>
2024-01-20 17:33:37 +0800

    add dragging on activity label
    tried adding smoothing for like 3 hours with no results ;)
    also i gave up having a line connecting

--- src/lib/components/homepage/ActivityFigure.svelte
+++ src/lib/components/homepage/ActivityFigure.svelte
@@ -12,16 +12,42 @@
 
   export let activities: ActivityLabel[];
 
-  export const activityLabel: Action<HTMLDivElement, ActivityLabel> = (
+  const distance = (...x: number[]) =>
+    Math.sqrt(x.map((n) => Math.pow(n, 2)).reduce((prev, curr) => prev + curr));
+  const activityLabel: Action<HTMLDivElement, ActivityLabel> = (
     node,
     { label, labelPosition, pinPosition }
   ) => {
-    // calculate line ends position
-    // create svg
-    // create line
-    // create pin (the circle)
-    // insert svg
     // create events (handle dragging)
+    node.addEventListener("mousedown", (e) => {
+      window.addEventListener("mousemove", onDrag);
+      window.addEventListener("mouseup", endDrag);
+
+      const initialPos = [node.offsetLeft, node.offsetTop];
+      const initialMousePos = [e.clientX, e.clientY];
+
+      function onDrag(e: MouseEvent) {
+        if (!e.buttons) endDrag();
+
+        let movedX = e.clientX - initialMousePos[0];
+        let movedY = e.clientY - initialMousePos[1];
+        const dist = distance(movedX, movedY);
+
+        const threshold = 100;
+        const rate = Math.min(dist / threshold, 1);
+        movedX = rate * threshold * (movedX / dist);
+        movedY = rate * threshold * (movedY / dist);
+
+        node.style.left = initialPos[0] + movedX + "px";
+        node.style.top = initialPos[1] + movedY + "px";
+      }
+
+      function endDrag() {
+        window.removeEventListener("mousemove", onDrag);
+        node.style.left = `${labelPosition[0]}rem`;
+        node.style.top = `${labelPosition[1]}rem`;
+      }
+    });
 
     return {
       update: ({ label, labelPosition, pinPosition }) => {
@@ -52,12 +78,20 @@
   </svg>
   {#each activities as act}
     <div
-      class="btn-accent absolute"
+      class="btn-accent absolute cursor-move select-none"
       style:left="{act.labelPosition[0]}rem"
       style:top="{act.labelPosition[1]}rem"
       use:activityLabel={act}
     >
       {act.label}
     </div>
+    <div
+      class="absolute h-2 w-2 rounded-full bg-accent"
+      style:left="{act.pinPosition[0]}rem"
+      style:top="{act.pinPosition[1]}rem"
+    />
   {/each}
 </div>
+
+<style>
+</style>
