dd053e093d572cbbb61aa534369e5076d1581be3
Eggrror404 <94736451+Eggrror404@users.noreply.github.com>
2024-02-06 23:45:15 +0800

    add styles for alerts

--- src/lib/components/svx/Alert.svelte
+++ src/lib/components/svx/Alert.svelte
@@ -1,18 +1,57 @@
 <script lang="ts">
-  const mTitles = {
-    "!NOTE": "Note",
-    "!TIP": "Tip",
-    "!IMPORTANT": "Important",
-    "!WARNING": "Warning",
-    "!CAUTION": "Caution"
-  };
+  import Information from "~icons/mdi/information-outline";
+  import Lightbulb from "~icons/mdi/lightbulb-outline";
+  import MessageAlert from "~icons/mdi/message-alert-outline";
+  import Alert from "~icons/mdi/alert-outline";
+  import AlertOctagon from "~icons/mdi/alert-octagon-outline";
 
-  export let title: keyof typeof mTitles;
+  export let title: keyof typeof titlesInfo;
+
+  const titlesInfo = {
+    "!NOTE": ["note", "Note", Information],
+    "!TIP": ["tip", "Tip", Lightbulb],
+    "!IMPORTANT": ["important", "Important", MessageAlert],
+    "!WARNING": ["warning", "Warning", Alert],
+    "!CAUTION": ["caution", "Caution", AlertOctagon]
+  } as const;
+
+  const alertInfo = titlesInfo[title];
 </script>
 
-<div class="markdown-alert border-l-2 px-4">
-  <p class="m-0 font-bold">
-    {mTitles[title]}
+<div class="markdown-alert border-l-4 px-4 markdown-alert-{alertInfo[0]} [&>*]:m-0">
+  <p class="icon-flex font-bold">
+    <svelte:component this={alertInfo[2]} />
+    {alertInfo[1]}
   </p>
   <slot />
 </div>
+
+<style>
+  .markdown-alert {
+    border-color: rgb(var(--alert-color));
+  }
+
+  .markdown-alert p {
+    color: rgb(var(--alert-color));
+  }
+
+  .markdown-alert-note {
+    --alert-color: var(--primary);
+  }
+
+  .markdown-alert-tip {
+    --alert-color: var(--accent);
+  }
+
+  .markdown-alert-important {
+    --alert-color: var(--accent);
+  }
+
+  .markdown-alert-warning {
+    --alert-color: var(--accent);
+  }
+
+  .markdown-alert-caution {
+    --alert-color: var(--accent);
+  }
+</style>
