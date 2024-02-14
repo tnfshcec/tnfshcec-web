61e3c84fa21d01d218c6552bbf09aa5fbe4f8bc1
Eggrror404 <94736451+Eggrror404@users.noreply.github.com>
2024-02-15 01:47:43 +0800

    add tags in PostCard, add tags filtering

--- src/routes/post/+page.svelte
+++ src/routes/post/+page.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
+  import { page } from "$app/stores";
   import CenteredPage from "$lib/components/CenteredPage.svelte";
   import PostCard from "$lib/components/homepage/PostCard.svelte";
   import * as m from "$paraglide/messages";
 
   export let data;
+
+  $: tags = $page.url.searchParams.get("tags")?.split(",");
 </script>
 
 <CenteredPage>
@@ -12,8 +15,10 @@
   </div>
 
   <div class="flex flex-col gap-4 py-4">
-    {#each data.posts as post}
-      <PostCard {post} />
+    {#each data.posts as post (post.slug)}
+      {#if !tags || tags.some((tag) => post.tags?.includes(tag))}
+        <PostCard displayTags {post} />
+      {/if}
     {/each}
   </div>
 </CenteredPage>
