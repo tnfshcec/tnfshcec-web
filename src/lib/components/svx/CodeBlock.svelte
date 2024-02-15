31319b05ffca1affce241ed251be897688cdf5e1
Eggrror404 <94736451+Eggrror404@users.noreply.github.com>
2024-02-15 13:17:40 +0800

    add language tag to codeblock

--- src/lib/components/svx/CodeBlock.svelte
+++ src/lib/components/svx/CodeBlock.svelte
@@ -2,6 +2,7 @@
   import ContentCopy from "~icons/mdi/content-copy";
   import Check from "~icons/mdi/text-box-check";
 
+  export let lang: string = "";
   export let title: string = "";
   export let nocopy: boolean = false;
 
@@ -22,15 +23,23 @@
   }
 </script>
 
-<div class="relative rounded-sm bg-secondary">
+<div class="group relative rounded-sm bg-secondary">
   {#if title}
     <span class="mx-4 inline-block py-1">{title}</span>
+  {/if}
+
+  {#if lang && !title}
+    <span
+      class="absolute right-2 text-text/40 opacity-100 transition-opacity group-hover:opacity-0"
+    >
+      {lang}
+    </span>
   {/if}
 
   {#if !nocopy}
     <button
       class="absolute right-2 top-2 rounded-sm border-text/20 bg-secondary/50
-             {title ? 'p-1' : 'border p-2'}"
+             {title ? 'p-1' : 'border p-2 opacity-0 transition-opacity group-hover:opacity-100'}"
       bind:this={copyButton}
       on:click={copy}
       aria-label="copy"
