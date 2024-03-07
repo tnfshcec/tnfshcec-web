6a9b3003e6ad38e68b07f7ef41aea14c5e5a6983
Eggrror404 <94736451+Eggrror404@users.noreply.github.com>
2024-03-07 17:45:14 +0000

    wip: fix postcard appearance

--- src/lib/components/homepage/PostCard.svelte
+++ src/lib/components/homepage/PostCard.svelte
@@ -13,21 +13,19 @@
 </script>
 
 <a
-  class="flex w-full flex-col gap-2 rounded bg-secondary px-4 py-6 transition-all hover:shadow-glow hover:shadow-secondary/80 motion-safe:hover:scale-[1.01]"
+  class="grid grid-cols-[auto_1fr] grid-rows-2 w-full gap-2 rounded bg-secondary px-4 py-6 transition-all hover:shadow-glow hover:shadow-secondary/80 motion-safe:hover:scale-[1.01]"
   href="{base}/post/{post.slug}"
   transition:fly={{ y: 10, duration: 250 }}
 >
-  <header class="flex w-full justify-between">
-    <div class="text-lg font-bold">{post.title}</div>
-    <div class="icon-flex text-primary/80">
-      {post.author ?? ""}
-      {post.author && post.date ? "/" : ""}
-      {date}
-      {#if post.pinned}
-        <Pin class="h-4 w-4 text-primary" />
-      {/if}
-    </div>
-  </header>
+  <header class="text-lg font-bold">{post.title}</header>
+
+  <div class="justify-end text-primary/80 row-span-2">
+    <div>{post.author ?? ""}</div>
+    <div>{date ?? ""}</div>
+    {#if post.pinned}
+      <Pin class="h-4 w-4 text-primary" />
+    {/if}
+  </div>
 
   {#if post.desc}
     <div class="text-text/80">{post.desc}</div>
@@ -36,9 +34,9 @@
   {#if displayTags && post.tags}
     <div class="flex gap-2 text-text/80">
       {#each post.tags as tag}
-        <a class="transition-colors hover:text-text hover:underline" href="{base}/post?tags={tag}"
-          >#{tag}</a
-        >
+        <a class="transition-colors hover:text-text hover:underline" href="{base}/post?tags={tag}">
+          #{tag}
+        </a>
       {/each}
     </div>
   {/if}
