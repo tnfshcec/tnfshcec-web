3ff335adc52d52624def4c8f22324267279e8090
Eggrror404 <94736451+Eggrror404@users.noreply.github.com>
2024-04-20 17:44:42 +0800

    add ORIGIN to playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,6 +2,7 @@ import type { PlaywrightTestConfig } from "@playwright/test";
 
 const config: PlaywrightTestConfig = {
   webServer: {
+    env: { ORIGIN: "https://www.tnfshcec.com" },
     command: "npm run build && npm run preview",
     port: 4173
   },
