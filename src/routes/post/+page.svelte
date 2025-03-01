c94dcdb7c61caed5e2ed3f8968755c66a6c75ce3
Eggrror404 <94736451+Eggrror404@users.noreply.github.com>
2025-03-02 02:10:39 +0800

    fix posts page & post card

--- src/routes/post/+page.svelte
+++ src/routes/post/+page.svelte
@@ -8,26 +8,32 @@
   let { data } = $props();
 
   /** current filtering tags */
-  let tags: string[] = $state([]);
-
-  $effect(() => {
-      const tagsParam = page.url.searchParams.get("tags") ?? "";
-      tags = tagsParam.split(",").filter((t) => t); // remove empty strings with filter()
+  let currentTags: string[] = $derived.by(() => {
+    const tagsParam = page.url.searchParams.get("tags") ?? "";
+    return tagsParam.split(",").filter((t) => t); // remove empty strings with filter()
   });
 
   /**
-   * Toggles the tags in searchParams
+   * Toggles the tags in currentTags
    *
    * For example:
    * `tagA,tagB` with `tabC` becomes `tagA,tagB,tagC`
    * `tagA,tagB,tagC` with `tagB` becomes `tagA,tagC`
    */
-  function getTagsParam(currentTags: string[], tag: string): string {
+  function toggleTag(tag: string): string[] {
     const index = currentTags.indexOf(tag);
 
     return index === -1
-      ? [...currentTags, tag].join(",") // add in the tag
-      : currentTags.toSpliced(index, 1).join(","); // remove the tag
+      ? [...currentTags, tag] // add in the tag
+      : currentTags.toSpliced(index, 1); // remove the tag
+  }
+
+  function getToggledHref(tag: string): string {
+    const newTags = toggleTag(tag);
+
+    return newTags.length === 0
+      ? "/post" // return without params
+      : `/post?tags=${newTags.join(",")}`;
   }
 </script>
 
@@ -39,11 +45,10 @@
   <!-- tags buttons for mobile -->
   <div class="flex w-full flex-wrap gap-2 lg:hidden">
     {#each data.tags as tag}
-      {@const tagsParam = getTagsParam(tags, tag)}
       <a
         class="btn-accent whitespace-nowrap
-          {tags.includes(tag) ? '' : 'border-accent/40 text-accent/60'}"
-        href="{base}/post{tagsParam === '' ? '' : '?tags=' + tagsParam}"
+          {currentTags.includes(tag) ? '' : 'border-accent/40 text-accent/60'}"
+        href={base + getToggledHref(tag)}
       >
         #{tag}
       </a>
@@ -52,32 +57,29 @@
 
   <div class="flex flex-col gap-4 py-4">
     {#each data.posts as post (post.slug)}
-      {#if tags.length === 0 || tags.every((tag) => post.tags?.includes(tag))}
+      {#if currentTags.length === 0 || currentTags.every((tag) => post.tags?.includes(tag))}
         <PostCard displayTags {post} />
       {/if}
     {/each}
   </div>
 
   {#snippet right()}
-  
-      <!-- tags buttons for desktop, on the right -->
-      {#if data.tags.size > 0}
-        <div class="sticky top-20 hidden w-max max-w-xs space-y-2 p-4 lg:block">
-          <p class="font-bold">{m.post_filter_tags()}</p>
-          <div class="flex flex-wrap gap-2">
-            {#each data.tags as tag}
-              {@const tagsParam = getTagsParam(tags, tag)}
-              <a
-                class="btn-accent whitespace-nowrap
-                  {tags.includes(tag) ? '' : 'border-accent/40 text-accent/60'}"
-                href="{base}/post{tagsParam === '' ? '' : '?tags=' + tagsParam}"
-              >
-                #{tag}
-              </a>
-            {/each}
-          </div>
+    <!-- tags buttons for desktop, on the right -->
+    {#if data.tags.size > 0}
+      <div class="sticky top-20 hidden w-max max-w-xs space-y-2 p-4 lg:block">
+        <p class="font-bold">{m.post_filter_tags()}</p>
+        <div class="flex flex-wrap gap-2">
+          {#each data.tags as tag}
+            <a
+              class="btn-accent whitespace-nowrap
+                  {currentTags.includes(tag) ? '' : 'border-accent/40 text-accent/60'}"
+              href={base + getToggledHref(tag)}
+            >
+              #{tag}
+            </a>
+          {/each}
         </div>
-      {/if}
-    
+      </div>
+    {/if}
   {/snippet}
 </CenteredPage>
