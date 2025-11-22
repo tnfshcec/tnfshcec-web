195a780cb2ef6b3e04743f13f1237b4ee281d218
Eggrror404 <94736451+Eggrror404@users.noreply.github.com>
2025-11-22 16:12:16 +0800

    increase playwright timeout

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,7 +4,8 @@ export default defineConfig({
   webServer: {
     env: { ORIGIN: "https://www.tnfshcec.com" },
     command: "npm run build && npm run preview",
-    port: 4173
+    port: 4173,
+    timeout: 120 * 1000 // double the default
   },
   testDir: "tests",
   testMatch: /(.+\.)?(test|spec)\.[jt]s/
